2f99fb54e872f4fbd3bf125365af3f88facc6b85
OnlyMid-sunquan <1585437938>
2017-08-31 16:06:46 +0800

    深浅复制对比

--- js/typeof.js
+++ js/typeof.js
@@ -4,3 +4,24 @@
 [typeof null,null instanceof Object];
 console.log(typeof null);
 console.log(null instanceof Object);
+
+var array = [1,2,3];
+console.log(typeof array);
+
+function myFunction() {
+
+}
+console.log(typeof myFunction);
+
+var myString = new String("i am fine");
+console.log(typeof myString);   //object
+console.log(myString.constructor == String);   //true
+console.log(myString instanceof String);    //true
+console.log(Object.prototype.toString.call(myString));    //true
+console.log(Object.prototype.toString(myString));    //true
+console.log(myString.toString());    //true
+
+console.log(Object.prototype.toString.call(array));
+
+
+
