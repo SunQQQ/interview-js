8c11aad38f0aac500368bbfeda450ad8a8998ab1
sunq <1585437938@qq.com>
2017-10-16 22:50:15 +0800

    regit

--- js/call.js
+++ js/call.js
@@ -1,16 +1,16 @@
 /**
  * Created by OnlyMid on 2017/3/29.
  */
-// var length = 10;
-// function fn() {
-//     console.log(this.length);
-// }
-// var obj = {
-//     length:5,
-//     method:function (fn) {
-//         fn();
-//         fn.call(obj);   //call的含义是：在obj环境中执行一遍函数fn的语句
-//     }
-// }
-// obj.method(fn);
+var length = 10;
+function fn() {
+    console.log(this.length);
+}
+var obj = {
+    length:5,
+    method:function (fn) {
+        fn();
+        fn.call(obj);   //call的含义是：在obj环境中执行一遍函数fn的语句
+    }
+}
+obj.method(fn);
 
